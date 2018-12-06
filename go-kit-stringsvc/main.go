f8e95f13219dc51c55f28fa1ab818ac1085aae0d
simone.albertini <>
2018-12-06 10:39:51 +0100

    prometheus instrumentation for string_service

--- go-kit-stringsvc/main.go
+++ go-kit-stringsvc/main.go
@@ -5,30 +5,65 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stdout)
+
+	// Prometheus instrumentation
+	fieldKeys := []string{"method", "error"}
+
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
+	// Service Handlers
 	var strService StringService
 	strService = stringService{}
 	strService = loggingMiddleware{logger: logger, next: strService}
+	strService = instrumentingMiddleware{
+		requestCount:   requestCount,
+		requestLatency: requestLatency,
+		countResult:    countResult,
+		next:           strService,
+	}
 
-	uppercaseEndpoint := makeUppercaseEndpoint(strService)
 	uppercaseHandler := httptransport.NewServer(
-		uppercaseEndpoint,
+		makeUppercaseEndpoint(strService),
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
-	countEndpoint := makeCountEndpoint(strService)
 	countHandler := httptransport.NewServer(
-		countEndpoint,
+		makeCountEndpoint(strService),
 		decodeCountRequest,
 		encodeResponse,
 	)
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	http.ListenAndServe(":8000", nil)
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Log("msg", "HTTP", "addr", ":8000")
+	logger.Log("err", http.ListenAndServe(":8000", nil))
 }
